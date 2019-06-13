da479e674746aa483b4f11ed28ac564e59cf2517
Chander bhushan <cbhushan1995@gmail.com>
2019-06-13 15:41:42 +0530

    How to merge two images like applying filters.

--- How To Merge Two Images/ViewController.swift
+++ How To Merge Two Images/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var filter2: UIImageView!
+    @IBOutlet weak var filter1: UIImageView!
+    @IBOutlet weak var orginalImage: UIImageView!
+    
+    var orginalImageVar:UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        orginalImageVar = orginalImage.image
     }
 
+    
+    func mergeTwoImage(originalImage:UIImage,filterImage:UIImage){
+        
+        let size = CGSize(width: originalImage.size.width, height: originalImage.size.height)
+        UIGraphicsBeginImageContext(size)
+        
+        let area = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        originalImage.draw(in: area)
+        filterImage.draw(in: area, blendMode: .normal, alpha: 1)
+        
+        var finalImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        orginalImage.image = finalImage
+    }
+    
+    
+    
+    @IBAction func applyFilterOne(_ sender: Any) {
+        mergeTwoImage(originalImage: orginalImageVar!, filterImage: filter1.image!)
+    }
+    
 
+    @IBAction func applyFiltreTwo(_ sender: Any) {
+        mergeTwoImage(originalImage: orginalImageVar!, filterImage: filter2.image!)
+    }
 }
 
